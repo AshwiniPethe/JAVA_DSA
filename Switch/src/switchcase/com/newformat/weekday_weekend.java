71f89f4d6fa7f1e8ee15c59e4093d3f380a79b16
AshwiniPethe <petheashwini123@gmail.com>
2025-02-14 00:20:27 +0530

    created new switch folder

---
+++ Switch/src/switchcase/com/newformat/weekday_weekend.java
@@ -0,0 +1,10 @@
+package switchcase.com.newformat;
+
+public class weekday_weekend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Switch/src/switchcase/com/oldformat/weekday_weekend.java
@@ -0,0 +1,10 @@
+package switchcase.com.oldformat;
+
+public class weekday_weekend {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
